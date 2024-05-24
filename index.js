b03a863ebaf3d7d6f012a277603ca5017c8f07bb
Md Abu Zafar <mdabuzafar78@gmail.com>
2024-05-24 08:15:37 +0530

    install nodemon and create a post endpoint

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const port = 4000;
 
 const app = express();
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("hello world");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("post request received");
 });
 
 app.listen(port, () => {
